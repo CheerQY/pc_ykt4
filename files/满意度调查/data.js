bfd595b0efa694e628704dda2e543c9cee8c8852
cheer <1479437773@qq.com>
2017-12-25 13:42:38 +0800

    访客，物业
    访客部分设计完成，物业管理部分设计进行中

---
+++ files/满意度调查/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,F,G,F,H,I,J,null,K,L,M,N,O,P,Q,L),R,_(),S,_(),T,_(U,[])),V,_(),W,_());}; 
+var b="url",c="满意度调查.html",d="generationDate",e=new Date(1514180483094.85),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="isappealed",k="conference_isappealed",l="page",m="packageId",n="e202d6d992aa4126bc58083e7eafb7f4",o="type",p="Axure:Page",q="name",r="满意度调查",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="center",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="near",G="imageVerticalAlignment",H="imageRepeat",I="auto",J="favicon",K="sketchFactor",L="0",M="colorStyle",N="appliedColor",O="fontName",P="Applied Font",Q="borderWidth",R="adaptiveStyles",S="interactionMap",T="diagram",U="objects",V="masters",W="objectPaths";
+return _creator();
+})());
